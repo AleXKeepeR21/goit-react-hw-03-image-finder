2e92fbdaf34c203093ff92f46d16ab21518ce46b
AleXKeepeR21 <106617654+AleXKeepeR21@users.noreply.github.com>
2023-01-03 00:36:33 +0200

    add modal

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,49 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import Modal from './Modal/Modal';
+
+export class App extends Component {
+  state = {
+    showModal: false,
+  };
+
+  toggleModal = () => {
+    this.setState(state => ({
+      showModal: !state.showModal,
+    }));
+  };
+  render() {
+    const { showModal } = this.state;
+    return (
+      <div
+      // style={{
+      //   height: '100vh',
+      //   display: 'flex',
+      //   justifyContent: 'center',
+      //   alignItems: 'center',
+      //   fontSize: 40,
+      //   color: '#010101',
+      // }}
+      >
+        <button type="button" onClick={this.toggleModal}>
+          open
+        </button>
+
+        {showModal && (
+          <Modal>
+            <h1>hello</h1>
+            <p>this is text</p>
+            <button type="button" onClick={this.toggleModal}>
+              cloce
+            </button>
+
+            {/* <img src={largeImageURL} alt="" /> */}
+          </Modal>
+          // {showModal && (
+          //   <Modal onClose={this.toggleModal}>
+          //     {/* <img src={largeImageURL} alt="" /> */}
+          //   </Modal>
+        )}
+      </div>
+    );
+  }
+}
